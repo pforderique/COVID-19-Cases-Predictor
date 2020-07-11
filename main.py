32696ee1073ecaee5d9db9c01e09151660f0a253
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-07-11 14:24:58 -0400

    started main screen but not working??

--- main.py
+++ main.py
@@ -15,7 +15,7 @@
 
 import pandas as pd
 import numpy as np
-from tkinter import Tk
+from tkinter import Tk, Label, PhotoImage
 
 #New York Times Data 
 NYT_US = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
@@ -33,3 +33,25 @@ DARK_GREY = '#454545'
 main_df = None # holds USA or individual df of choice
 state_df = None #holds ALL states
 county_df = None #hold ALL counties
+
+def startupScreen():
+    #window
+    startWindow = Tk()
+    startWindow.title('COVID-19 Tracker')
+    startWindow.geometry('600x370')
+    startWindow.config(bg=DARK_BURGANDY)
+    icon = PhotoImage(file ='images\COVID-19-virus-white.png')
+    startWindow.iconphoto(False, icon)
+
+    #window title
+    titleLabel = Label(startWindow, text="COVID-19 Cases Predictor", font=("Times New Roman Bold", 25),bg=DARK_BURGANDY,fg='white')
+    titleLabel.pack(pady=10)#grid(column=0, row=0, padx=13,pady=10)
+    
+    #logo
+    main_image = PhotoImage(file ='images\COVID-19-virus.png')
+    main_image_lbl = Label(image=main_image)
+    main_image_lbl.image = main_image
+    main_image_lbl.place(relx=0.04, rely=0.2)
+
+if __name__ == "__main__":
+    startupScreen()
