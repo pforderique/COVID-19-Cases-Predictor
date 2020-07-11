f97be41edd6f5107b3b214a35ec07a2845484087
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-07-11 13:38:34 -0400

    First commit.

---
+++ main.py
@@ -0,0 +1,14 @@
+##########################################
+# COVID-19 Cases Predictor 
+#
+# Purpose: To show COVID-19 cases and predict using 
+#           regression models
+# @author Piero Orderique
+#
+# TODO:
+#   1) research more about tkinter. 
+#   2) Practice reading in data from google on jupyter
+#   3) Start building basic startup GUI
+#   4) Figure out how to display graphs
+#   5) Implement the machine learning part into program
+##########################################
